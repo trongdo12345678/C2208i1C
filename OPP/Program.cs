f4530cb35c8851e2ed4403488273c98a393b6202
Trọng Đỗ <nguyendotrong2003@gmail.com>
2023-11-24 14:41:29 +0700

    xong bài học

--- OPP/Program.cs
+++ OPP/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using OPP;
+
+//Student stu = new(1,"trongdo", true ,new DateTime(2003,04,01));
+//object initializer = > class không cần tạo constroter làm gì
+Student stu3 = new()
+{
+    Id = 1,
+    //Gender = false,
+    FullName = "trọng",
+    Dob = DateTime.Now
+};
+Console.WriteLine(stu3);
+
+
+
+//Id là Property của stu
+
+//stu.Id = 1; //set
+//Console.WriteLine(stu.Id);
+//stu.Fullname = "trọng Đỗ";
+//Console.WriteLine(stu.Fullname);//get
+//stu.Gender = true;
+//Console.WriteLine(stu.Gender);
+//stu.Dob = "01/01/2002";
+//Console.WriteLine();
---
+++ OPP/Student.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OPP;
+internal class Student
+{
+
+
+    #region code co ban
+    //internal class Student
+    //{
+    //    //các field
+    //    int id;
+    //    string? fullname;
+    //    //tự các field sec thành luôn thuộc tính
+    //    public bool Gender { get; set; } //=> auto property phải là public
+    //    public DateTime Dob { get; set; }//=> auto property cách này dùng để đi làm
+
+
+    //    public void Hi()
+    //    {
+    //        id = 1;
+    //        Console.WriteLine(id);
+    //        Gender = true;
+    //        Console.WriteLine(Gender);
+    //    }
+
+    //    //constructor nhấn chuột phải chọn ddầu tiên để khai báo constructor
+    //    public Student() { }
+
+    //    public Student(int id, string? fullname, bool gender, DateTime dob)
+    //    {
+    //        this.Id = id;
+    //        this.Fullname = fullname;
+    //        this.Gender = gender;
+    //        this.Dob = dob;
+    //    }
+
+    //    //cấc property 
+    //    //khác với java c# có cách viết riêng mà không dùng get set như java
+
+    //    public int Id
+    //    {
+    //        get { return id; }
+    //        set { id = value; }
+    //    }
+    //    public string? Fullname // cái style này dùng để viết mobile
+    //    {
+    //        get => fullname;
+    //        set => fullname = value;
+    //    }
+
+    //    public override string? ToString()
+    //    {
+    //        return base.ToString();
+    //    }
+    //}
+
+    #endregion
+
+
+    public int Id { get; set; }
+    public string FullName { get; set; }
+    public int Gender { get; set; }
+    public DateTime Dob { get; set; }
+
+    //có thể không dùng contructer
+    public Student() { }
+
+    public Student(int id, string fullName, int gender, DateTime dob)
+    {
+        Id = id;
+        FullName = fullName;
+        Gender = gender;
+        Dob = dob;
+    }
+
+    public override string ToString()
+    {
+        return $"{{{nameof(Id)}={Id.ToString()}, {nameof(FullName)}={FullName}, {nameof(Gender)}={Gender.ToString()}, {nameof(Dob)}={Dob.ToString()}}}";
+    }
+}
